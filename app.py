6b9f221896b044aa64905855caf87a015bbba4f2
sanjin-lebesgue <sanjin@lebesgue.io>
2023-02-02 07:16:17 +0100

    authentication

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 import pandas as pd
 from loguru import logger
 
+from authenticate import authenticate
 from utils.enum import get_enum_values
 from tabs.ctr_tab import ctr_tab
 from tabs.mlm_tab import mlm_tab
@@ -14,26 +15,27 @@ from tabs.mlm_tab import mlm_tab
 load_dotenv()
 
 API_HOST = os.getenv("API_HOST")
-
-
 st.set_page_config(layout="wide")
 
+authenticator = authenticate()
 
-with st.sidebar:
+if st.session_state["authentication_status"]:
+    authenticator.logout("Logout", "sidebar")
 
-    st.markdown("<h1 style='text-align: center;'>⭐SanJin EnGin⭐</h1>", unsafe_allow_html=True)
+    with st.sidebar:
 
-    main_tab = option_menu(
-        menu_title="Main menu",
-        options=["CTR", "Masked language"],
-        menu_icon="menu-app",
-        icons=["mouse", "mask"],
-        default_index=0,
-    )
+        st.markdown("<h1 style='text-align: center;'>⭐SanJin EnGin⭐</h1>", unsafe_allow_html=True)
 
+        main_tab = option_menu(
+            menu_title="Main menu",
+            options=["CTR", "Masked language"],
+            menu_icon="menu-app",
+            icons=["mouse", "mask"],
+            default_index=0,
+        )
 
-if main_tab == "CTR":
-    ctr_tab()
+    if main_tab == "CTR":
+        ctr_tab()
 
-if main_tab == "Masked language":
-    mlm_tab()
+    if main_tab == "Masked language":
+        mlm_tab()
